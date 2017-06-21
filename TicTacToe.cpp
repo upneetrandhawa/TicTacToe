2ee97ee62df5b63619c3952106000bd4b0b8dba4
upneetrandhawa <upneet.randhawa@mail.utoronto.ca>
2017-06-21 13:23:05 -0400

    Resolved debug errors, Game working properly.
    Added methods isOccupied()

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,8 +1,7 @@
-
 #include <unistd.h>
 #include <string>
 #include <iostream>
-#include <conio.h>
+
 
 using namespace std;
 int winnerId = 0;
@@ -11,289 +10,312 @@ int scoreCard[9] = {0};
 
 class TicTacToe{
 private:
-	string playerName;
-	bool isCurrentPlayer = false;
+    string playerName;
+    bool isCurrPlayer = false;
 
 public:
-	string getPlayerName(){
-		return playerName;
-	}
+    string getPlayerName(){
+        return playerName;
+    }
 
-	bool isCurrentPlayer(){
-		return isCurrentPlayer;
-	}
+    bool isCurrentPlayer(){
+        return isCurrPlayer;
+    }
 
-	void setPlayerName(string name){
-		playerName = name;
-	}
+    void setPlayerName(string name){
+        playerName = name;
+    }
 
-	void setCurrentPlayer(bool val){
-		isCurrentPlayer = val;
-	}
+    void setCurrentPlayer(bool val){
+        isCurrPlayer = val;
+    }
 };
 
+bool checkWinner(TicTacToe *players);
+void printBoard(TicTacToe *players);
+void printHorizontalPartition();
+void printVerticalPartition();
+bool checkHorizontal(int playerId, int position);
+bool checkVertical(int playerId, int position);
+bool checkDiagonal(int playerId, int position);
+bool noPosition(TicTacToe *players);
+bool isOccupied(int position);
+
+
 int main(){
-	cout<<"------------------TIC TAC TOE------------------"<<
-			<<"\n\n"<<endl;
-	TicTacToe *players = new TicTacToe[2];
+    cout<<"------------------TIC TAC TOE------------------"
+            <<"\n\n"<<endl;
+    TicTacToe *players = new TicTacToe[2];
     string nameBuf;
-	cout << "Enter Player Name 1: ";
-	cin >> nameBuf;
-	players[0].setPlayerName(nameBuf);
-	cout << "Enter Player Name 2: ";
-	cin >> nameBuf;
-	players[1].setPlayerName(nameBuf);
+    cout << "Enter Player Name 1: ";
+    cin >> nameBuf;
+    players[0].setPlayerName(nameBuf);
+    cout << "Enter Player Name 2: ";
+    cin >> nameBuf;
+    players[1].setPlayerName(nameBuf);
 
-	printBoard();
-	bool winner = false;
+    printBoard(players);
+    bool winner = false;
 
-	while (!winner() && !noPosition()){
+    while (!winner && !noPosition(players)){
 
-		for (int i = 0; i<2 ; i++){
-			int val;
-			cout<<"Player"<<i+1<<" "<< players[i].getPlayerName<<" input an empty position: ";
-			cin >> val;
+        for (int i = 0; i<2 && winner != true; i++){
+            int val;
+            cout<<"Player"<<i+1<<" "<< players[i].getPlayerName()<<" input an empty position: ";
+            cin >> val;
 
-			scoreCard[val-1] = i+1;
+            while(isOccupied(val)){
+                cout<<"Position already taken!!"<<endl;
+                cout<<"Player"<<i+1<<" "<< players[i].getPlayerName()<<" input an empty position: ";
+                cin >> val;
+            }
 
-			players[i].setCurrentPlayer(true);
+            scoreCard[val-1] = i+1;
 
-			if(i==1){
-				players[i-1].setCurrentPlayer(false);
-			}
-			else{
-				players[i+1].setCurrentPlayer(false);
-			}
+            players[i].setCurrentPlayer(true);
 
-			printBoard();
-			winner = checkWinner();
-;		}
+            if(i==1){
+                players[i-1].setCurrentPlayer(false);
+            }
+            else{
+                players[i+1].setCurrentPlayer(false);
+            }
 
-	}
-	if (winnerId!==0)
-	cout<<"\n\n------------------Player "<<players[winnerId-1].getPlayerName
-		<<" WON!!!------------------\n\n"<<endl;
+            printBoard(players);
+            winner = checkWinner(players);
+;       }
 
-	else if (noPosition())
-		cout<<"\n\n------------------Nobody WON------------------\n\n"<<endl;
+    }
+    if (winnerId!=0)
+    cout<<"\n\n------------------Player "<<players[winnerId-1].getPlayerName()
+        <<" WON!!!------------------\n\n"<<endl;
+
+    else if (noPosition(players))
+        cout<<"\n\n------------------Nobody WON------------------\n\n"<<endl;
 
 
+return 1;
+}
+bool checkWinner(TicTacToe *players){
+
+    int currPlayer;
+        if(players[0].isCurrentPlayer()){
+            currPlayer = 1;
+        }
+        else
+            currPlayer = 2;
+
+    for (int pos = 0 ; pos < 9 ; pos++){
+        
+        if (scoreCard[pos] == currPlayer) {
+        
+
+            bool horizontal = checkHorizontal(currPlayer, pos);
+            if (horizontal){
+                winnerId = currPlayer;
+                return horizontal;
+            }
+    
+            bool vertical = checkVertical(currPlayer, pos);
+            if (vertical){
+                winnerId = currPlayer;
+                return vertical;
+            }
+    
+            bool diagonal = checkDiagonal(currPlayer, pos);
+            if (diagonal){
+                winnerId = currPlayer;
+                return diagonal;
+            }
+        }
+
+    }
+    
+    return false;
 
 }
-bool checkWinner(){
+void printBoard(TicTacToe *players){
+    printHorizontalPartition();
+    for (int i = 0; i < 9 ; i++){
 
-	int currPlayer;
-		if(players[0].isCurrentPlayer){
-			currPlayer = 1;
-		}
-		else
-			currPlayer = 2;
+            printVerticalPartition();
+            
+            if(scoreCard[i]!=0){
+                cout<<"P"<<scoreCard[i]<<"   ";
+            }
+            else{
+                cout<<i+1<<"    ";
+            }
+            
+            
 
-	for (int pos = 0 ; pos < 9 ; pos++){
-
-		bool horizontal = checkHorizontal(currPlayer, pos);
-		if (horizontal){
-			winnerId = currPlayer;
-			return horizontal;
-		}
-
-		bool vertical = checkVertical(currPlayer, pos);
-		if (vertical){
-			winnerId = currPlayer;
-			return vertical;
-		}
-
-		bool diagonal = checkDiagonal(currPlayer, pos);
-		if (diagonal){
-			winnerId = currPlayer;
-			return diagonal;
-		}
-
-	}
-	
-	return false;
-
-}
-void printBoard(){
-	printHorizontalPartition();
-	for (int i = 0; i < 9 ; i++){
-
-			printVerticalPartition();
-		    
-			if(scoreCard[i]!=0){
-				cout<<"P"<<scoreCard[i]<<"   ";
-			}
-			else{
-				cout<<i+1<<"    ";
-			}
-			
-			printVerticalPartition();
-
-			if((i+1) %3 ==0 )
-				printHorizontalPartition();
-	}
+            if((i+1) %3 ==0 ){
+                printVerticalPartition();
+                cout<<"\n";
+                printHorizontalPartition();
+             
+            }
+    }
 
 }
 void printHorizontalPartition(){
-	cout<<"----------+---------+----------";
+    cout<<"----------+---------+----------"<<endl;
 }
 void printVerticalPartition(){
-	cout<<"|    ";
+    cout<<"|    ";
 }
 
 bool checkHorizontal(int playerId, int position){
 
-	if (scoreCard[position]!= playerId)
-		return false;
+    
 
-	int location = (position+1)%3;
+    int location = (position+1)%3;
 
-	if (location == 1) {
-		if (scoreCard[position+1] == playerId){
-			if (scoreCard[position+2] == playerId){
-				return true;
-			}
-			else 
-				return false;
-		}
-		else 
-			return false;
+    if (location == 1) {
+        if (scoreCard[position+1] == playerId){
+            if (scoreCard[position+2] == playerId){
+                return true;
+            }
+            else 
+                return false;
+        }
+        else 
+            return false;
 
-	}
+    }
 
-	else if (location == 2) {
-		if (scoreCard[position+1] == playerId){
-			if(scoreCard[position -1] == playerId){
-				return true;
-			}
-			else
-				return false;
-		}
+    else if (location == 2) {
+        if (scoreCard[position+1] == playerId){
+            if(scoreCard[position -1] == playerId){
+                return true;
+            }
+            else
+                return false;
+        }
 
-		else
-			return false;
+        else
+            return false;
 
-	}
+    }
 
-	else if (location == 0){
+    else if (location == 0){
 
-		if (scoreCard[position-1] == playerId){
-			if(scoreCard[position -2] == playerId){
-				return true;
-			}
-			else
-				return false;
-		}
+        if (scoreCard[position-1] == playerId){
+            if(scoreCard[position -2] == playerId){
+                return true;
+            }
+            else
+                return false;
+        }
 
-		else
-			return false;
+        else
+            return false;
 
-	}
+    }
+    return false;
 
 }
 
 bool checkVertical(int playerId, int position){
-	if (scoreCard[position]!= playerId)
-		return false;
+   
 
-	int location = (position+1)%3;
+    int location = (position+1)%3;
 
-	if (location == 1) {
-		if (scoreCard[position+3] == playerId){
-			if (scoreCard[position+6] == playerId){
-				return true;
-			}
-			else 
-				return false;
-		}
-		else 
-			return false;
+    if (location == 1) {
+        if (scoreCard[position+3] == playerId){
+            if (scoreCard[position+6] == playerId){
+                return true;
+            }
+            else 
+                return false;
+        }
+        else 
+            return false;
 
-	}
+    }
 
-	else if (location == 2) {
-		if (scoreCard[position+3] == playerId){
-			if(scoreCard[position -3] == playerId){
-				return true;
-			}
-			else
-				return false;
-		}
+    else if (location == 2) {
+        if (scoreCard[position+3] == playerId){
+            if(scoreCard[position -3] == playerId){
+                return true;
+            }
+            else
+                return false;
+        }
 
-		else
-			return false;
+        else
+            return false;
 
-	}
+    }
 
-	else if (location == 0){
+    else if (location == 0){
 
-		if (scoreCard[position-3] == playerId){
-			if(scoreCard[position -3] == playerId){
-				return true;
-			}
-			else
-				return false;
-		}
+        if (scoreCard[position-3] == playerId){
+            if(scoreCard[position -3] == playerId){
+                return true;
+            }
+            else
+                return false;
+        }
 
-		else
-			return false;
+        else
+            return false;
 
-	}
-
+    }
+    return false;
 }
 
 bool checkDiagonal(int playerId, int position){
-	if (scoreCard[position]!= playerId)
-		return false;
+    
 
-	//temporary hardcoded solution
-	if(position == 0){
-		if(scoreCard[position+4] == playerId){
-			if(scoreCard[position+8] == playerId)
-				return true;
-			else
-				return false;
-		}
-		else return false;
-	}
-	else if(position == 2){
-		if(scoreCard[position+2] == playerId){
-			if(scoreCard[position+4] == playerId)
-				return true;
-			else
-				return false;
-		}
-		else return false;
-	}
-	else if(position == 4){
-		if(scoreCard[position+4] == playerId){
-			if(scoreCard[position-4] == playerId)
-				return true;
-			else
-				return false;
-		}
-		else return false;
-	}
-	else if(position == 6){
-		if(scoreCard[position-2] == playerId){
-			if(scoreCard[position-4] == playerId)
-				return true;
-			else
-				return false;
-		}
-		else return false;
-	}
-	else if(position == 8){
-		if(scoreCard[position-4] == playerId){
-			if(scoreCard[position-8] == playerId)
-				return true;
-			else
-				return false;
-		}
-		else return false;
-	}
-	else
-		return false;
+    //temporary hardcoded solution
+    if(position == 0){
+        if(scoreCard[position+4] == playerId){
+            if(scoreCard[position+8] == playerId)
+                return true;
+            else
+                return false;
+        }
+        else return false;
+    }
+    else if(position == 2){
+        if(scoreCard[position+2] == playerId){
+            if(scoreCard[position+4] == playerId)
+                return true;
+            else
+                return false;
+        }
+        else return false;
+    }
+    else if(position == 4){
+        if(scoreCard[position+4] == playerId){
+            if(scoreCard[position-4] == playerId)
+                return true;
+            else
+                return false;
+        }
+        else return false;
+    }
+    else if(position == 6){
+        if(scoreCard[position-2] == playerId){
+            if(scoreCard[position-4] == playerId)
+                return true;
+            else
+                return false;
+        }
+        else return false;
+    }
+    else if(position == 8){
+        if(scoreCard[position-4] == playerId){
+            if(scoreCard[position-8] == playerId)
+                return true;
+            else
+                return false;
+        }
+        else return false;
+    }
+    else
+        return false;
 } 
 // ----------+---------+----------
 // |    1    |    2    |    3    |    
@@ -302,11 +324,18 @@ bool checkDiagonal(int playerId, int position){
 // ----------+---------+----------
 // |    7    |    8    |    9    |
 // ----------+---------+----------
-bool noPosition(){
-	bool noPosition = true;
-	for (int i = 0 ; i< 9 && noPosition = true ; i++){
-		if(scoreCard[i] == 0)
-			noPosition = false;
-	}
-	return noPosition;
+bool noPosition(TicTacToe *players){
+    bool noPosition = true;
+    for (int i = 0 ; i< 9 && noPosition == true ; i++){
+        if(scoreCard[i] == 0)
+            noPosition = false;
+    }
+    return noPosition;
+}
+
+bool isOccupied(int position){
+    if(scoreCard[position-1]!=0)
+        return true;
+
+    return false;
 }
